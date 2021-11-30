8dcb9232732b6c2dbbcbdbd5369160be2a507736
Jessica Crabb <jessica.ann.crabb@gmail.com>
2021-11-30 00:33:42 -0500

    updated game logic and scenes added music

--- Unity/Finished Object/Finished Object/Assets/Scripts/WinScenario.cs
+++ Unity/Finished Object/Finished Object/Assets/Scripts/WinScenario.cs
@@ -18,15 +18,28 @@ public class WinScenario : MonoBehaviour
     public GameObject Slot7;
     public GameObject Slot8;
     public string nextScene;
-    
+    public bool isImg1On;
+    public Image img1;
+    public bool isImg2On;
+    public Image img2;
+    public bool isImg3On;
+    public Image img3;
+    public bool isImg4On;
+    public Image img4;   
 
     // Start is called before the first frame update
     void Start()
     {
         CraftButton.SetActive(false);
         Debug.Log(Slot1.transform.childCount);
-
-
+        img1.enabled = false;
+        isImg1On = false;
+        img2.enabled = false;
+        isImg2On = false;
+        img3.enabled = false;
+        isImg3On = false;
+        img4.enabled = false;
+        isImg4On = false;
 
     }
 
@@ -41,15 +54,35 @@ public class WinScenario : MonoBehaviour
             Slot7.transform.childCount + 
             Slot8.transform.childCount);
 
-
-        if (Slot1.transform.childCount + 
+        
+        if (Slot5.transform.childCount == 1 && isImg1On == false){
+            Debug.Log("hi");
+            img1.enabled = true;
+            isImg1On = true;
+        }
+        else if (Slot6.transform.childCount == 1 && isImg2On == false){
+            Debug.Log("yo");
+            img2.enabled = true;
+            isImg2On = true;
+        }
+        else if (Slot7.transform.childCount == 1 && isImg3On == false){
+            Debug.Log("ye");
+            img3.enabled = true;
+            isImg3On = true;
+        }
+        else if (Slot8.transform.childCount == 1 && isImg4On == false){
+            Debug.Log("ah");
+            img4.enabled = true;
+            isImg4On = true;
+        }
+        else if (Slot1.transform.childCount + 
             Slot2.transform.childCount + 
             Slot3.transform.childCount + 
             Slot4.transform.childCount + 
             Slot5.transform.childCount + 
             Slot6.transform.childCount + 
             Slot7.transform.childCount + 
-            Slot8.transform.childCount > 3){
+            Slot8.transform.childCount > 7){
             CraftButton.SetActive (true);
         }
         else{
